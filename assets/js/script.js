283d0a893443c0f07e217e46c826232f32d3090b
charlottegracia <1086120@ucn.dk>
2022-07-28 23:13:38 +0200

    js clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
 function menu(action) {
-    console.log("hej");
-    console.log(action);
     if (action == "open") {
         document.getElementById("mobileMenu").style.display = "block";
-        
     } else {
         document.getElementById("mobileMenu").style.display = "none";
     }
